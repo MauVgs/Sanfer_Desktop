19da29cd3f528f2d2c309542caf37d002fc486b0
MauVgs <43446262+MauVgs@users.noreply.github.com>
2019-12-05 14:22:43 -0600

    Primer commit

---
+++ common_files/header.php
@@ -0,0 +1,36 @@
+<header>
+
+    <nav class="navbar">
+        <div id="logo">
+            <a href="#">
+                <img width="112px" src="/assets/images/logo.png" alt="Sanfer">
+            </a>
+        </div>
+        <ul>
+            <li>
+                <a href="#">Nosotros</a>
+            </li>
+            <li>
+                <a href="#">Productos</a>
+            </li>
+            <li>
+                <a href="#">Servicios de diagnóstico</a>
+            </li>
+            <li>
+                <a href="#">Noticias</a>
+            </li>
+            <li>
+                <a href="#">Contacto</a>
+            </li>
+        </ul>
+    </nav>
+    <nav class="menu-botones">
+        <div class="idioma">
+            <a href="#"><i class="material-icons">public</i>ESP<i class="material-icons">expand_more</i></a>
+        </div>
+        <div class="consulta">
+            <a href="">Consulta de resultados</a>
+        </div>
+    </nav>
+
+</header>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sanfer</title>
+    <?php include "common_files/styles.php";?>
+</head>
+<body>
+    <?php include "common_files/header.php"; ?>
+
+    <main id="main" class="bg_banner_home">
+        <section class="home__header">
+            <div class="home_welcome">
+                <div class="home_title">
+                    <h1>Cuidado, bienestar <br> y salud animal</h1>
+                </div>
+                <div class="home_parr">
+                    <p>
+                        <a href="">Líneas de producto</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="home_animales">
+            <div class="rectangulo_rojo"></div>
+            <div class="rectangulo_blanco"></div>
+            <div class="animales">
+                <article class="animal">
+                        <div class="img-animal">
+                            <img src="/assets/images/home/gallina.jpeg" alt="">
+                        </div>
+                        <div class="select-animal">
+                            <a href=""><i class="material-icons">arrow_forward</i></a>
+                        </div>
+                        <div class="desc-animal">
+                            <p>Aves</p>
+                        </div>
+                </article>
+                <article class="animal">
+                        <div class="img-animal">
+                            <img src="/assets/images/home/cerdo.jpg" alt="">
+                        </div>
+                        <div class="select-animal">
+                            <a href=""><i class="material-icons">arrow_forward</i></a>
+                        </div>
+                        <div class="desc-animal">
+                            <p>Aves</p>
+                        </div>
+                </article>
+                <article class="animal">
+                        <div class="img-animal">
+                            <img src="/assets/images/home/vaca.jpg" alt="">
+                        </div>
+                        <div class="select-animal">
+                            <a href=""><i class="material-icons">arrow_forward</i></a>
+                        </div>
+                        <div class="desc-animal">
+                            <p>Aves</p>
+                        </div>
+                </article>
+                <article class="animal">
+                        <div class="img-animal">
+                            <img src="/assets/images/home/caballo.jpg" alt="">
+                        </div>
+                        <div class="select-animal">
+                            <a href=""><i class="material-icons">arrow_forward</i></a>
+                        </div>
+                        <div class="desc-animal">
+                            <p>Aves</p>
+                        </div>
+                </article>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
